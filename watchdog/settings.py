6348be82af90f5c11c6bd635794b99963305f4a5
theognis1002 <theognis1002@gmail.com>
2020-12-15 16:28:23 -0500

    updated config and installed uwsgi

--- watchdog/settings.py
+++ watchdog/settings.py
@@ -27,7 +27,7 @@ sys.path.insert(0, os.path.join(BASE_DIR, "apps"))
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
