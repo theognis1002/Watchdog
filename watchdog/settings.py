852523d9fe73a44e62ed1f0f4a1dc4efff090a7d
theognis1002 <theognis1002@gmail.com>
2020-12-15 16:41:39 -0500

    readying app for deployment

--- watchdog/settings.py
+++ watchdog/settings.py
@@ -128,7 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 # WATCHDOG SCRAPER SETTINGS
 SCRAPER_DELAY = 60 * 3
--- watchdog/urls.py
+++ watchdog/urls.py
@@ -1,8 +1,14 @@
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.conf import settings
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("panel.urls")),
     path("", include("users.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
